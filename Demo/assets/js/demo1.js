2b30f041b5bc423a02e5ced31d5f1985e51a6011
shixiaomu58 <1468443120@qq.com>
2017-01-16 15:36:38 +0800

    1月16号

---
+++ Demo/assets/js/demo1.js
@@ -0,0 +1,65 @@
+var app=angular.module('myapp',[]);
+app.controller('food',['$scope','$http',function($scope,$http){
+    $scope.index=1;
+    var response='';
+    $http.get('assets/json/demo.js',{PageSize:5,PageIndex:1}).success(function(res){
+        response=res;
+        $scope.items=response.items;       
+    })
+    //     $scope.setValue=function(){
+    //         $http.post('assets/json/demo.js',{PageSize:5,PageIndex:2}).success(function(res){
+    //     response=response.concat(res);
+    //     $scope.items=response;       
+    // })
+    //     }
+
+
+
+}])
+
+
+    // var spanPro=function(name.addtime){
+    //  Name:name,
+    //  AddTime:addtime
+    // };
+    // // var spanPro={'Name':"国产",'AddTime':'2016-01-02'};
+    // // var spanPro1={'Name':"国产2",'AddTime':'2016-01-02'};
+    // var span1=new spanPro('国产','2016-01-02');
+    // var span2=new spanPro('国产','2016-01-02');
+    // $scope.index=1;
+    // $scope.ProduceList=[
+    // {'Name':1240,'Price':1023,'Span':[span1,span2]}
+    // ];
+    // var result={
+    //  TotalCount:20,
+    //  PageIndex:2,
+    //  PageSize:4,
+    //  PageCount:5,
+    //  items:[
+    //  {
+    //      pname: '可口可乐',
+    //      price: 3.0,
+    //      pdate: new Date(2015, 8, 3),
+    //      isOnSale: false
+    //  },
+    //  {
+    //      pname: '百事可乐',
+    //      price: 2.5,
+    //      pdate: new Date(2015, 7, 3),
+    //      isOnSale: true
+    //  },
+    //  {
+    //      pname: '芬达',
+    //      price: 2.8,
+    //      pdate: new Date(2015, 5, 2),
+    //      isOnSale: true
+    //  },
+    //  {
+    //      pname: '七喜',
+    //      price: 2.3,
+    //      pdate: new Date(2015, 2, 15),
+    //      isOnSale: true
+    //  }
+    //  ]
+    // };
+    // $scope.items=result.items;
